0af336d7907fa757b17ab326248a309b7ce901c9
Unknown <shandy1806@gmail.com>
2018-06-08 17:03:24 +0300

    user gets alert when trying to group notes with no title

--- src/app/retro/retro.component.ts
+++ src/app/retro/retro.component.ts
@@ -18,6 +18,7 @@ export class RetroComponent implements OnInit, OnDestroy {
   @Output() goBack = new EventEmitter();
   notesByGroups: { group: string, notes: Note[] }[] = [];
   sprint = '';
+  showAlertMessage: boolean;
   private notes: Note[];
   private user: AppUser;
   private userSubscription: Subscription;
@@ -33,33 +34,47 @@ export class RetroComponent implements OnInit, OnDestroy {
 
     this.userSubscription =
       this.dataService.getUser().subscribe(user => {
-      this.user = user;
-      this.teamSubscription = this.dataService.getTeam(this.user.team)
-        .subscribe((doc: {sprints: string[]}) => {
-          this.team = doc;
-          if (this.team.sprints) {
-            this.sprint = this.oldSprint ? this.oldSprint : this.team.sprints[this.team.sprints.length - 1];
-            this.notes = this.team && this.team[this.sprint] ? this.team[this.sprint] : [];
-            this.notesByGroups = [];
-            this.mapNotesToGroups();
-          }
-        });
-    });
+        this.user = user;
+        this.teamSubscription = this.dataService.getTeam(this.user.team)
+          .subscribe((doc: { sprints: string[] }) => {
+            this.team = doc;
+            if (this.team.sprints) {
+              this.sprint = this.oldSprint ? this.oldSprint : this.team.sprints[this.team.sprints.length - 1];
+              this.notes = this.team && this.team[this.sprint] ? this.team[this.sprint] : [];
+              this.notesByGroups = [];
+              this.mapNotesToGroups();
+            }
+          });
+      });
   }
 
   private configureDragula() {
     this.dragulaService.setOptions('first-bag', {
-      moves: function (el: any, container: any, handle: any): any {
+      moves: function (el, container, handle): boolean {
         return el.tagName !== 'INPUT';
-      }
+      },
+      accepts: function (el, target, source, sibling): boolean {
+        if (source !== target) {
+          const title = target.children[0].value;
+          if (!title) {
+            this.showAlertMessage = true;
+            setTimeout(function () {
+              this.showAlertMessage = false;
+            }.bind(this), 10000);
+            return false;
+          }
+        }
+        return true;
+      }.bind(this)
     });
 
     this.dragulaSubscription =
       this.dragulaService.drop.subscribe((value) => {
         const destination = value[2];
         const source = value[3];
+        const inputTitle = destination.children[0];
         this.removeGroup(source);
-        this.saveGroup(destination.children[0]);
+        this.saveGroup(inputTitle);
       });
   }
 
@@ -88,17 +103,17 @@ export class RetroComponent implements OnInit, OnDestroy {
       if (!note.group) {
         this.notesByGroups.push({
           group: '', notes:
-            [{ text: note.text, by: note.by, at: note.at, group: '' }]
+            [{text: note.text, by: note.by, at: note.at, group: ''}]
         });
       } else {
         const groupIndex = this.notesByGroups.map(x => x.group).indexOf(note.group);
         if (groupIndex > -1) {
           this.notesByGroups[groupIndex].notes.push(
-            { text: note.text, by: note.by, group: note.group, at: note.at });
+            {text: note.text, by: note.by, group: note.group, at: note.at});
         } else {
           this.notesByGroups.push({
             group: note.group, notes:
-              [{ text: note.text, by: note.by, group: note.group, at: note.at }]
+              [{text: note.text, by: note.by, group: note.group, at: note.at}]
           });
         }
       }
