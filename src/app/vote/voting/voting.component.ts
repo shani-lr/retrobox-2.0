1dfb27048eee26bad12be9d84614e5e4def5aadd
Unknown <shandy1806@gmail.com>
2018-06-19 22:38:24 +0300

    user can change his vote

--- src/app/vote/voting/voting.component.ts
+++ src/app/vote/voting/voting.component.ts
@@ -12,8 +12,9 @@ import { Note } from '../../core/models/note.model';
 })
 export class VotingComponent implements OnInit, OnDestroy {
   showErrorMessage: boolean;
-  voted: boolean;
+  voteSubmitted: boolean;
   notesByItems: { item: string, notes: Note[], votes: number }[] = [];
+  hasVoted: boolean;
   private totalVotes = 3;
   private user: AppUser;
   private team: { sprints: string[], vote: [{ item: string, votes: number, user: string }] };
@@ -30,6 +31,7 @@ export class VotingComponent implements OnInit, OnDestroy {
       this.dataService.getUser().subscribe(user => {
         this.user = user;
         this.subscriptions.push(this.dataService.getVoteType().subscribe(voteType => this.voteType = voteType));
+
         this.subscriptions.push(this.dataService.getTeam(this.user.team)
           .subscribe((doc: { sprints: string[], vote: [{ item: string, votes: number, user: string }] }) => {
             this.team = doc;
@@ -38,6 +40,7 @@ export class VotingComponent implements OnInit, OnDestroy {
               this.notes = this.team && this.team[this.sprint] ? this.team[this.sprint] : [];
               this.notesByItems = [];
               this.mapNotesToItems();
+              this.populateVotes();
             }
           }));
       }));
@@ -56,15 +59,18 @@ export class VotingComponent implements OnInit, OnDestroy {
     this.notesByItems.forEach(noteByItem => noteByItem.votes = 0);
     this.totalVotes = 3;
     this.showErrorMessage = false;
+    this.team.vote = this.team.vote.filter(item => item.user !== this.user.name);
+    this.subscriptions.push(this.dataService.updateTeam(this.user.team, this.team).subscribe());
   }
 
-  onVote() {
+  onSubmitVote() {
     this.notesByItems.forEach(noteByItem => {
       if (noteByItem.votes) {
         this.team.vote.push({item: noteByItem.item, votes: noteByItem.votes, user: this.user.name});
       }
     });
-    this.subscriptions.push(this.dataService.updateTeam(this.user.team, this.team).subscribe(() => this.voted = true));
+
+    this.subscriptions.push(this.dataService.updateTeam(this.user.team, this.team).subscribe(() => this.voteSubmitted = true));
   }
 
   ngOnDestroy() {
@@ -73,29 +79,48 @@ export class VotingComponent implements OnInit, OnDestroy {
 
   private mapNotesToItems() {
     if (this.voteType === 'category') {
-      this.notes.map(note => {
-        if (note.group) {
-          const groupIndex = this.notesByItems.map(x => x.item).indexOf(note.group);
-          if (groupIndex > -1) {
-            this.notesByItems[groupIndex].notes.push(
-              {text: note.text, by: note.by, group: note.group, at: note.at});
-          } else {
-            this.notesByItems.push({
-              item: note.group,
-              notes: [{text: note.text, by: note.by, group: note.group, at: note.at}],
-              votes: 0
-            });
-          }
-        }
-      });
+      this.mapNotesToCategories();
     } else {
-      this.notes.map(note => {
-        this.notesByItems.push({
-          item: `${note.text.substring(0, 30)}${note.text.length > 30 ? '...' : ''}`,
-          notes: [{text: note.text, by: note.by, group: note.group, at: note.at}],
-          votes: 0
-        });
-      });
+      this.mapNotesToNoteItems();
     }
+  }
+
+  private mapNotesToCategories() {
+    this.notes.map(note => {
+      if (note.group) {
+        const groupIndex = this.notesByItems.map(x => x.item).indexOf(note.group);
+        if (groupIndex > -1) {
+          this.notesByItems[groupIndex].notes.push(
+            {text: note.text, by: note.by, group: note.group, at: note.at});
+        } else {
+          this.notesByItems.push({
+            item: note.group,
+            notes: [{text: note.text, by: note.by, group: note.group, at: note.at}],
+            votes: 0
+          });
+        }
+      }
+    });
+  }
+
+  private mapNotesToNoteItems() {
+    this.notes.map(note => {
+      this.notesByItems.push({
+        item: note.text,
+        notes: [{text: note.text, by: note.by, group: note.group, at: note.at}],
+        votes: 0
+      });
+    });
+
+  }
+
+  private populateVotes() {
+    this.team.vote.forEach((userVote: { item: string, votes: number, user: string }) => {
+      if (userVote.user === this.user.name) {
+        this.hasVoted = true;
+        const itemIndex = this.notesByItems.map(x => x.item).indexOf(userVote.item);
+        this.notesByItems[itemIndex].votes = userVote.votes;
+      }
+    });
   }
 }
